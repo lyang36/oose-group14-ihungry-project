fa8a7ee6fe14e14fb4e7f087289f94b6bc323566
parkavi <psritha1@jhu.edu>
2012-11-02 20:54:12 -0400

    add restaurant project

---
+++ iHungryRestaurant/src/edu/jhu/cs/oose/group14/restaurant/model/LoginHandler.java
@@ -0,0 +1,83 @@
+package edu.jhu.cs.oose.group14.restaurant.model;
+
+
+import java.security.MessageDigest;
+
+import edu.jhu.cs.oose.fall2012.group14.ihungry.internet.*;
+
+import java.security.MessageDigest;
+import java.math.BigInteger;
+
+/**
+ * Helper class for the model of ihungry Vendor application. Handles the error 
+ * checking and connecting to server tasks.
+ * 
+ * @author parkavi
+ *
+ */
+public class LoginHandler {
+	
+	private String username;
+	private String password;
+	private MessageDigest md;
+	private InternetClient client;
+	//Person r;
+	
+	/**
+	 *Constructor for LoginHandler class. 
+	 */
+	
+	public LoginHandler(String uname, String pwd){
+		
+		username = uname;
+		password = pwd;
+		try{
+			md = MessageDigest.getInstance("MD5");
+		}
+		catch(Exception e){
+			System.out.println("An exception occured");
+		}
+		client = new InternetClient();
+	}
+	
+	/**
+	 * Converts username and password into md5 hash string and connects to 
+	 * server with a JSON format message.Retrievs the message sent back by
+	 * server.
+	 * 
+	 * @return command string returned from the server
+	 */
+	
+	public String attemptLogin(){
+		
+		String result ="";
+		
+		md.update(username.getBytes());
+		String usernamemd5 = new BigInteger(1,md.digest()).toString(16);
+		md.update(password.getBytes());
+		String passwordmd5 = new BigInteger(1,md.digest()).toString(16);
+		String cmdString = usernamemd5 + passwordmd5 + CommunicationProtocol.BUSI_LOGIN;
+		//System.out.println(cmdString);
+		String supplyInfo = "{\"usernamemd5\":" + usernamemd5 + "\";" +
+						    "\"username\":" + username + "\";" +
+						    "\"password\":" + password + "\"}";
+		String message = CommunicationProtocol.packMsg(CommunicationProtocol.packStr(cmdString,supplyInfo));
+		//System.out.println("original message is " + message);
+		
+		String a = CommunicationProtocol.construcSendingStr(
+				CommunicationProtocol.FB_SIGN_NAME, CommunicationProtocol.FB_SIGN_PASSWD, 
+				CommunicationProtocol.LOGIN, "{\"lyang\": \"123456\"}");
+		try{
+			result = CommunicationProtocol.getRequestFromReceivedStr(client.sendAndGet(a,30000));
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception occured while sending and getting");
+		}
+		
+		System.out.println("result is " + result);
+		return result;
+	}
+
+}
---
+++ iHungryRestaurant/src/edu/jhu/cs/oose/group14/restaurant/model/ihungryRestaurantModel.java
@@ -0,0 +1,121 @@
+package edu.jhu.cs.oose.group14.restaurant.model;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import edu.jhu.cs.oose.fall2012.group14.ihungry.internet.CommunicationProtocol;
+import edu.jhu.cs.oose.group14.restaurant.gui.ihungryRestaurantGui;
+import edu.jhu.cs.oose.project.group14.ihungry.model.*;
+import java.util.*
+;
+
+/**
+ * ihungryRestaurantModel class is the model class for ihungry vendor 
+ * application. It uses other helper classes like LoginHandler to handle
+ * specific functions.
+ */
+
+public class ihungryRestaurantModel {
+	
+	private ihungryRestaurantGui gui;
+	ArrayList<JButton> listOfEdit;
+    ArrayList<JButton> listOfDelete;
+    ArrayList<JTextField> listOfItemNames;
+    ArrayList<JTextField> listOfDescription ;
+    ArrayList<JTextField> listOfPrice;
+	private int itemNo =0;
+	
+	/*
+	 * Constructor for ihungryVendorController
+	 */
+	
+	public ihungryRestaurantModel(ihungryRestaurantGui gui){
+		this.gui = gui;
+		listOfEdit = gui.getEditButton();
+	    listOfDelete = gui.getDeleteButton();
+	    listOfItemNames = gui.getItemNames();
+	    listOfDescription = gui.getDescription();
+	    listOfPrice = gui.getPrice();
+		gui.getLogin().addActionListener(new LoginListener());
+	}
+	
+	/**
+	 * LoginListener class implements the actionPerformed method checking for
+	 * the user credentials and allowing the user to log into the application.
+	 * 
+	 * @author parkavi
+	 *
+	 */
+	
+	class LoginListener implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			
+			LoginHandler loginHandle = new LoginHandler(gui.getUsernameLogin().getText(),new String(gui.getPasswordLogin().getText()));
+			String result = loginHandle.attemptLogin();
+			System.out.println(result);
+			System.out.println(CommunicationProtocol.NO_SUCH_COMMAND);
+			
+			gui.displayOrderGui();
+           //gui.getmenuScrollBar().addAdjustmentListener(new MenuScrollBarListener());
+        
+			for(int i=0;i<5;i++)
+			{
+				listOfEdit.get(i).addActionListener(new EditButtonListener() );
+				listOfDelete.get(i).addActionListener(new DeleteButtonListener() );
+			}
+            
+		}
+	}
+	
+	/**
+	 * EditButtonListener class implements the actionPerformed method checking for 
+	 * the user credentials and allowing the user to log into the application.
+	 * 
+	 * @author parkavi
+	 *
+	 */
+	
+	
+	class EditButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) 
+		{
+			System.out.println(event.getSource());
+			int ind = listOfEdit.indexOf(event.getSource());
+			
+			JTextField name = listOfItemNames.get(ind);
+		    JTextField description = listOfDescription.get(ind);
+		    JTextField price = listOfPrice.get(ind);
+		     
+			if(listOfEdit.get(ind).getText()=="Save")
+			{
+				Price p = new Price(Double.valueOf(price.getText()));
+			    //Item newItem = new Item(Integer.toString(++itemNo),name.getText(),description.getText(),p);
+	     
+			     //call a method to send this item to server to be stored in the database
+			     
+			     name.setEditable(false);
+			     description.setEditable(false);
+			     price.setEditable(false);
+			     listOfEdit.get(ind).setText("Edit");
+			}
+			else if(listOfEdit.get(ind).getText()=="Edit")
+			{
+				name.setEditable(true);
+		        description.setEditable(true);
+		        price.setEditable(true);
+		        listOfEdit.get(ind).setText("Save");
+				
+			}
+		}
+	}
+	
+	class DeleteButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			
+		}
+	}
+	
+	
+}
