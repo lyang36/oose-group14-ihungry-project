feda66a059a82027db39afb76b891baed8d494fb
parkavi <psritha1@jhu.edu>
2012-11-12 15:33:59 -0500

    changes to Menu.java

--- iHungryLib/src/edu/jhu/cs/oose/project/group14/ihungry/model/Menu.java
+++ iHungryLib/src/edu/jhu/cs/oose/project/group14/ihungry/model/Menu.java
@@ -1,7 +1,9 @@
 package edu.jhu.cs.oose.project.group14.ihungry.model;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.io.Serializable;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -10,25 +12,86 @@ import org.json.JSONObject;
  * @author group14
  *
  */
-public class Menu implements JSONHandler{
+public class Menu implements JSONHandler,Serializable{
+	
+	public static final String KEY_REST_ID = "restId";
+	public static final String KEY_MENU_SIZE = "menuSize";
+
+	
+	private List<Item> items = new ArrayList<Item>();
+	private String restId;
+	private int menuSize;
+	
 	public Menu(){
 		
 	}
-	public Menu( ArrayList<Item> items){
+	
+	public Menu( String restId,List<Item> items){
+		this.restId = restId;
+		this.items = items;
 		
 	}
 	
-	public ArrayList<Item> getItems(){
-		return null;
+	public List<Item> getItems(){
+		return items;
 	}
+	
+	public Item getItemAt(int i){
+		return items.get(i);
+	}
+	
+	public void addItems(List<Item> items){
+		this.items.addAll(items);
+	}
+	
+	public void addItem(Item item){
+		this.items.add(item);
+	}
+	
+	public void setItem(int i, Item item){
+		this.items.set(i,item);
+	}
+	
+	public void replaceMenu(List<Item> items){
+		this.items = items;
+	}
+	
 	@Override
 	public JSONObject getJSON() {
-		// TODO Auto-generated method stub
-		return new JSONObject();
+		JSONObject jsonobj = new JSONObject();
+		try {
+			jsonobj.put(KEY_REST_ID, restId);
+			jsonobj.put(KEY_MENU_SIZE, menuSize);
+						
+			for(int i=0; i<this.menuSize; i++){
+				jsonobj.put(i+"", items.get(i).getJSON());
+			}
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return jsonobj;
 	}
+	
+	
 	@Override
 	public void parseFromJSONObject(JSONObject jsonobj) {
-		// TODO Auto-generated method stub
+		try {
+			restId = jsonobj.getString(KEY_REST_ID);
+			menuSize = Integer.parseInt(jsonobj.getString(KEY_MENU_SIZE));
+			
+			items = new ArrayList<Item>();
+			for(int i=0; i<menuSize; i++){
+				Item item = new Item();
+				item.parseFromJSONObject(jsonobj.getJSONObject(i+""));
+				items.add(item);
+			}
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}	
 		
 	}
+		
 }
+
