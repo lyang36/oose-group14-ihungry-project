6a30ecaa941945fbd70f5f493f827724e49e38ba
SuNFloWer <SuNFloWer@orange.wireless.jhu.edu>
2012-11-29 13:42:12 -0500

    modelimpl delete some trash code

--- AndroidiHungry/src/edu/jhu/cs/oose/project/group14/ihungry/androidclientmodel/AndroidClientModelImpl.java
+++ AndroidiHungry/src/edu/jhu/cs/oose/project/group14/ihungry/androidclientmodel/AndroidClientModelImpl.java
@@ -94,21 +94,7 @@ public class AndroidClientModelImpl implements AndroidClientModel {
 	}
 
 	public Customer getCustomerInfo(String username, String password) {
-		
-		/* Assume below is Generated on the server side */
-/*		ContactInfo cInfo = new ContactInfo("Shang Zhao",
-				new LocationInfo("Johns Hopkins University, Baltimore, MD, 21218"),
-				"911-911-9999", "443-343-1111", "szhao12@jhu.edu",
-				"1989-12-11", new Icon());
-		AccountInfo aInfo = new AccountInfo("szhao12", "12345");
-		Customer customer = new Customer();
-		customer.setAccountInfo(aInfo);
-		customer.setContactInfo(cInfo);
-		String responseFromServer = customer.getJSON().toString();
-*/
-		/* -----------> transmitting from server -----------> */
 
-		/* <----------- received by client <----------- */
 		String sendStr = CommunicationProtocol.construcSendingStr(
 				MD5.getNameMd5(username), MD5.getMd5(password),
 				CommunicationProtocol.CUS_LOGIN, "");
@@ -299,36 +285,6 @@ public class AndroidClientModelImpl implements AndroidClientModel {
 		/* 2. Call internetClient.sendAndGet to get response from server */
 		/* 3. If valid => parse the information to Menu object */
 
-/*		Item item1 = new Item("i001", "Chicken with Broccoli", 4.5, new Rating(
-				4.0, 10), new Album());
-		Item item2 = new Item("i002", "Assorted Mixed Vegetable", 4.65,
-				new Rating(4.4, 11), new Album());
-		Item item3 = new Item("i003", "Shrimp with Lobster Sauce", 4.95,
-				new Rating(4.3, 12), new Album());
-		Item item4 = new Item("i004", "Chicken with Cashew Nuts", 5.05,
-				new Rating(4.1, 13), new Album());
-		Item item5 = new Item("i005", "B-B-Q Spare Ribs", 5.25, new Rating(
-				3.95, 14), new Album());
-		Item item6 = new Item("i006", "Skewered Beef", 4.5,
-				new Rating(4.8, 15), new Album());
-		Item item7 = new Item("i007", "Wonton Soup", 1.5, new Rating(4.5, 16),
-				new Album());
-		Item item8 = new Item("i008", "House Special Soup", 5.50, new Rating(
-				4.7, 17), new Album());
-		List<Item> items = new ArrayList<Item>();
-		items.add(item1);
-		items.add(item2);
-		items.add(item3);
-		items.add(item4);
-		items.add(item5);
-		items.add(item6);
-		items.add(item7);
-		items.add(item8);
-		Menu menuServer = new Menu(restId, items); */
-		/* menuObj_Str should be inside the SupplyInfo of the response */
-//		String menuObj_Str = menuServer.getJSON().toString();
-		/* -----------> transmitting from server -----------> */
-
 		AccountInfo bus_acc = new AccountInfo();
 		bus_acc.setId(restId);
 				
@@ -349,16 +305,6 @@ public class AndroidClientModelImpl implements AndroidClientModel {
 			e.printStackTrace();
 		}
 		
-		/* <----------- received by client <----------- */
-/*		JSONObject obj = null;
-		try {
-			Log.i("[Menu_info]", menuObj_Str + "");
-			obj = new JSONObject(menuObj_Str);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		menuClient.parseFromJSONObject(obj);
-*/
 		return menu_received;
 	}
 
@@ -409,47 +355,6 @@ public class AndroidClientModelImpl implements AndroidClientModel {
 		 * Generate a sending message containing custId to server and if
 		 * response are valid => convert to a List<Order> object
 		 */
-/*
-		List<Order> orders = new ArrayList<Order>();
-
-		Item item1 = new Item("i001", "Chicken with Broccoli", 4.5, new Rating(
-				4.0, 10), new Album());
-		Item item2 = new Item("i002", "Assorted Mixed Vegetable", 4.65,
-				new Rating(4.4, 11), new Album());
-		Item item3 = new Item("i003", "Shrimp with Lobster Sauce", 4.95,
-				new Rating(4.3, 12), new Album());
-		List<OrderItem> o_items1 = new ArrayList<OrderItem>();
-		o_items1.add(new OrderItem(item1, 2));
-		o_items1.add(new OrderItem(item2, 1));
-		o_items1.add(new OrderItem(item3, 1));
-		Order order1 = new Order("o002", "c003", "r004", 1, o_items1);
-
-		Item item4 = new Item("i004", "Chicken with Cashew Nuts", 5.05,
-				new Rating(4.1, 13), new Album());
-		Item item5 = new Item("i005", "B-B-Q Spare Ribs", 5.25, new Rating(
-				3.95, 14), new Album());
-		Item item6 = new Item("i006", "Skewered Beef", 4.5,
-				new Rating(4.8, 15), new Album());
-		Item item7 = new Item("i007", "Wonton Soup", 1.5, new Rating(4.5, 16),
-				new Album());
-		List<OrderItem> o_items2 = new ArrayList<OrderItem>();
-		o_items2.add(new OrderItem(item4, 1));
-		o_items2.add(new OrderItem(item5, 1));
-		o_items2.add(new OrderItem(item6, 3));
-		o_items2.add(new OrderItem(item7, 2));
-		Order order2 = new Order("o001", "c003", "r009", 3, o_items2);
-
-		Item item8 = new Item("i008", "House Special Soup", 5.50, new Rating(
-				4.7, 17), new Album());
-		List<OrderItem> o_items3 = new ArrayList<OrderItem>();
-		o_items3.add(new OrderItem(item8, 2));
-		Order order3 = new Order("o003", "c003", "r012", 2, o_items3);
-
-		orders.add(order1);
-		orders.add(order2);
-		orders.add(order3);
-*/
-		/* Get restaurant Account Infos */
 		List<Order> my_orders = new ArrayList<Order>();
 		OrderQuerier querier = new OrderQuerier();
 		querier.setCusID("lyang");
